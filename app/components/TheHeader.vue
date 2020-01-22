ecb16304ee14b17014d9960f47e2ea55df601ecc
toshihirokato <nanamin.104@icloud.com>
2020-01-22 21:34:31 +0900

    edit element-ui add export default

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -3,7 +3,7 @@
     <el-menu-item index="1" style="pointer-events: none;">
       Nuxt Diary App
     </el-menu-item>
-    <el-menu-item :route="{ path: '/posts' }" index="2">
+    <el-menu-item :route="{ path: '/posts/' }" index="2">
       投稿一覧
     </el-menu-item>
 
