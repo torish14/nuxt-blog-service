24e5ea4dee04a7384afb0afe5a7cb61ffe6cd70d
toshihirokato <nanamin.104@icloud.com>
2020-01-22 13:21:13 +0900

    投稿一覧ページの作成

---
+++ app/pages/posts/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="container posts-page">
+    <el-card>
+      <div slot="header" class="clearfix">
+        <span>新着投稿</span>
+      </div>
+      <el-table :data="showPosts" class="table" style="width: 100%;">
+        <el-table-column prop="title" label="タイトル" />
+        <el-table-column prop="user.id" label="投稿者" width="180" />
+        <el-table-column prop="created_at" label="投稿日時" width="240" />
+      </el-table>
+    </el-card>
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    showPosts () {
+      return [
+        {
+          id: '001',
+          title: 'How to development Nuxt.js Application',
+          body: 'Hello World1',
+          created_at: '2020/01/22 13:00',
+          user: {
+            id: 'torish14'
+          }
+        },
+        {
+          id: '002',
+          title: 'Development Nuxt.js Application to Heroku',
+          body: 'Hello World2',
+          created_at: '2020/01/22 13:30',
+          user: {
+            id: 'torish14'
+          }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.posts-page .el-table__row {
+  cursor: pointer;
+}
+</style>
