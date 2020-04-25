746e8e291fbb39cb94601d23f49b959ca19b3e95
toshihirokato <nanamin.104@icloud.com>
2020-04-25 23:33:26 +0900

    no-ssrタグをclient-onlyタグに変更

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -1,22 +1,21 @@
 <template>
-  <el-menu :router="true" mode="horizontal">
-    <el-menu-item index="1" style="pointer-events: none;">
+  <el-menu mode="horizontal" :router="true">
+    <el-menu-item index="1" style="pointer-events:none;">
       Nuxt Diary App
     </el-menu-item>
-    <el-menu-item :route="{ path: '/posts/' }" index="2">
+    <el-menu-item index="2" :route="{ path: '/posts/' }">
       投稿一覧
     </el-menu-item>
 
-    <no-ssr>
-      <!-- 自分がログインしているか確認するために、ログイン中はユーザーの情報を表示できるようにする -->
-      <el-menu-item :route="{ path: `/users/${user.id}` }" v-if="user" index="4" style="float: right;">
+    <client-only>
+      <el-menu-item v-if="user" index="4" style="float:right;" :route="{ path: `/users/${user.id}` }">
         <span>{{ user.id }}</span>
       </el-menu-item>
-      <el-menu-item :route="{ path: '/' }" v-else index="4" style="float: right;">
+      <el-menu-item v-else index="4" style="float: right;" :route="{ path: '/' }">
         <span>ログイン</span>
       </el-menu-item>
-    </no-ssr>
-    <el-menu-item :route="{ path: '/posts/new' }" index="5" style="float: right;">
+    </client-only>
+    <el-menu-item index="5" style="float: right;" :route="{ path: '/posts/new' }">
       新規登録
     </el-menu-item>
   </el-menu>
@@ -31,3 +30,7 @@ export default {
   }
 }
 </script>
+
+<style>
+
+</style>
